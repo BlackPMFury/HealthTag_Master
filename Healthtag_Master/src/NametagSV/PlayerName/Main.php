c4e5b75172e450434a9a8c13cc4e80fc007935b0
BlackPMFury <40240050+BlackPMFury@users.noreply.github.com>
2019-07-04 12:04:54 +0700

    Add files via upload

---
+++ Healthtag_Master/src/NametagSV/PlayerName/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NametagSV\PlayerName;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\{Player, Server};
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\untils\Config;
+use jojoe7777\FormAPI;
+
+class Main extends PluginBase implements Listener{
+  public $tag = "Nametag•System";
+  public $config;
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info($this->tag . "Enable Plugin....");
+    $this->nt = new Config($this->getDataFolder() . "Nametag.yml", Config::YAML, []);
+    $this->nt->save();
+  }
+  
+  public function onJoin(PlayerJoinEvent $ev){
+    $player = $ev->getPlayer();
+    $name = $player->getName();
+    $heal = $player->getHealth();
+    $player->getNameTag($ev->setNametag("Health:". $heal));
+  }
+}
